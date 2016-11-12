3c8a120b805256fdd882d10fcd9467134ac6c2a0
Julie <julieregistr@gmail.com>
2016-11-12 20:55:59 +0100

    Full page

--- js/main.js
+++ js/main.js
@@ -1,24 +1,50 @@
 /**
  * Created by yuliya on 11.11.16.
  */
+
 angular.module('myweb', ['ngRoute', 'ui.router'])
     .config(function($stateProvider, $urlRouterProvider) {
         $stateProvider
             .state('home', {
                 url: '/',
-                templateUrl: 'home.html'
+                views: {
+                    'twoSides': {
+                        templateUrl: 'home.html'
+                    },
+                    'oneSide': {
+                        templateUrl: 'homeOne.html'
+                    }
+                }
             })
             .state('about', {
                 url: '/about',
-                templateUrl: 'about.html'
+                views: {
+                    'twoSides': {
+                        templateUrl: 'about.html'
+                    },
+                    'oneSide': {
+                        templateUrl: 'about.html'
+                    }
+                }
             });
     })
     .run(function ($state) {
         $state.go('home');
     })
     .controller('mainCtrl', ['$scope', '$state', function ($scope, $state) {
+        $scope.$on('$viewContentLoaded', function() {
+            var canvas = document.getElementById('canvas')[0].getContext('2d');//angular.element(document.querySelector('#canvas'));
+            /*if(canvas!==null) {
+                var context = canvas[0].getContext('2d');
+                console.log(canvas);
+            }
+            else {
+                console.log('canvas is  null');
+            }*/
+        });
+        //var context = canvas.getContext('2d');
+
         $scope.test = 'Test angular';
-        $scope.goToAbout = function () {
-            $state.go('about');
-        };
+
 }]);
+
